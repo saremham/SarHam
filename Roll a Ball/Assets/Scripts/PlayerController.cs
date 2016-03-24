8589dac9dc5d30492623cb353e11a4b6a331a889
Sarah Hamilton <sarahhamilton@bennington.edu>
2016-03-23 23:43:46 -0400

    finished roll a ball tutorial and downloaded tank tutorial

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate ()
@@ -24,9 +31,20 @@ public class PlayerController : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) 
 	{
-		if (other.gameObject.CompareTag ("Pick Up"))
+		if (other.gameObject.CompareTag ( "Pick Up"))
 		{
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 10)
+		{
+			winText.text = "You Win!";
 		}
 	}
 }
