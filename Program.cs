b0ac3e353439afab7a5549a41e64b85bd3e5c34f
Sarah Hamilton <sarahhamilton@bennington.edu>
2016-04-25 10:28:27 -0400

    SuperSimpleCalculator

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SuperSimpleCalculator
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.Write ("What is your name?");
+			string name = Console.ReadLine (); 
+			Console.WriteLine ("Hello" + name); 
+
+			Console.Write ("Enter first number: ");
+			string firstNumberString = Console.ReadLine ();
+			int firstNumber = int.Parse (firstNumberString); 
+
+			Console.Write ("Enter second number: ");
+			string secondNumberString = Console.ReadLine (); 
+			int secondNumber = int.Parse (secondNumberString);
+
+			Console.Write ("Enter operator: ");
+			string operatorString = Console.ReadLine ();
+			Console.Write (firstNumberString + operatorString + secondNumberString + "=");
+			if (operatorString == "*") { 
+				Console.Write (firstNumber * secondNumber);
+			} else if (operatorString == "+") {
+				Console.Write (firstNumber + secondNumber);
+			} else if (operatorString == "/") {
+				Console.Write (firstNumber / secondNumber);
+			} else if (operatorString == "-") {
+				Console.Write (firstNumber - secondNumber);
+			}
+
+				
+		}
+	}
+}
